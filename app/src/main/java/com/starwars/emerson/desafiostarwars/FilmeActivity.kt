de462de9c5e184e11cec048a5c41e3431e7e8549
Emerson Almeida Matos <e.matos@aluno.ufabc.edu.br>
2018-11-12 12:12:31 -0200

    Activity de detalhes do filme

---
+++ app/src/main/java/com/starwars/emerson/desafiostarwars/FilmeActivity.kt
@@ -0,0 +1,12 @@
+package com.starwars.emerson.desafiostarwars
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filme)
+    }
+}
